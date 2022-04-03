ff6f57d0dc1464dd07463cbc8dd5c4b929a2ebab
Vijay Shanmugam <vrswizards2015@gmail.com>
2022-04-02 22:51:08 -0400

    add headers and update url

--- client/components/about.js
+++ client/components/about.js
@@ -9,7 +9,7 @@ export default class About {
       innerHTML:
     'HyperCall is a 3D video calling solution created by <a target="_blank" href="https://www.vijayrs.com">Vijay Shanmugam</a>, <a target="_blank" href="https://blog.preterite.me/">Patrick Zhang</a>, & <a target="_blank" href="https://mci.sh/">Michael Ilie</a>. '
     + 'It is a web application that allows you to call people in 3D space, and share your camera with them. '
-    + 'It is built on top of <a target="_blank" href="https://google.github.io/mediapipe/>">MediaPipe</a>, and WebRTC. '
+    + 'It is built on top of <a target="_blank" href="https://google.github.io/mediapipe">MediaPipe</a>, and WebRTC. '
   + 'Source: <a target="_blank" href="https://github.com/Jbolt01/HyperCall">https://github.com/Jbolt01/HyperCall</a>' });
 
     addEl(this.el, this.back, this.icon, this.modal);
